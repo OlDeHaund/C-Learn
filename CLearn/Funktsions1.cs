ab666584fc6d58e6877f9c8b9b85a65fe8eaf35c
OlDeHaund <ravil10096@gmail.com>
2025-06-16 09:37:50 +0300

    Add files via upload

---
+++ CLearn/Funktsions1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Clearn
+{
+    internal class Funktsions1
+    {
+        /*
+        public static float Kalkulaator(int arv1, int arv2)
+        {
+            float kalkulator = 0;
+            kalkulator = arv1 * arv2;
+            return kalkulator;
+        }
+        public static string Jukudef(string tekst, int vanus)
+        {
+            if (tekst.ToLower() == "juku")
+            {
+                Console.WriteLine("Lahme kinno!");
+                try
+                {
+                    if (vanus <= 0 || vanus > 100) // || - or, && - and
+                    {
+                        return ("Viga");
+                    }
+                    else if (vanus > 0 && vanus <= 6)
+                    {
+                        return ("Tasuta");
+                    }
+                    else if (vanus <= 15)
+                    {
+                        return ("Lastepilet");
+                    }
+                    else if (vanus <= 65)
+                    {
+                        return ("TaisPilet");
+                    }
+                    else if (vanus <= 100)
+                    {
+                        return ("Sooduspilet!");
+                    }
+                }
+                catch (Exception e)
+                {
+                    return ("Viga");
+                }
+            }
+            else
+            {
+                return("Olen hoivatud!");
+            }
+        }
+        */
+
+    }
+}
